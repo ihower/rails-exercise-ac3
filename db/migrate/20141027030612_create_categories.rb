417e1400241c66c9fa7f83cab9728329e0d66ef7
Wen-Tien Chang <ihower@gmail.com>
2014-10-27 11:45:17 +0800

    Add category, location, attendee models

---
+++ db/migrate/20141027030612_create_categories.rb
@@ -0,0 +1,17 @@
+class CreateCategories < ActiveRecord::Migration
+  def change
+    create_table :categories do |t|
+
+      t.string :name
+      t.integer :position
+
+      # rake db:rollback
+
+      t.timestamps
+    end
+
+     add_column :events, :category_id, :integer
+     add_index :events, :category_id
+
+  end
+end
